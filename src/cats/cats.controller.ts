441b06ade98eefdca0f632b498b183a6dc1dbe79
yaoqilin <1392675219@qq.com>
2019-11-13 14:26:57 +0800

    添加路由

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Header, HttpCode, Param, Post, Query, Redirect, Req} from '@nestjs/common';
+import {Request} from 'express';
+import {CreateCatDto} from "./create-cat.dto";
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    @Post()
+    @HttpCode(204) // 状态码
+    @Header('Cache-Control', 'none')
+    // create(): string {
+    //     return '这个操作是添加一个新的cat';
+    // }
+    @Get('ab*cd') // 路由通配符
+    @Redirect('https://nestjs.com', 301)
+    findAll(@Req() request: Request): string {
+        return '这个操作是返回所有的cats';
+    }
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com', 302) // 重定向
+    getDocs(@Query('version') version) {
+        if (version && version === '5') {
+            return { url: 'https://docs.nestjs.com/v5'};
+        }
+    }
+    @Get(':id') // 路由参数
+    findOne(@Param() params): string {
+        console.log(params.id);
+        return `这个操作是返回一个${params.id}cat`;
+    }
+    // @Get(':id')
+    // findOne(@Param('id') id): string {
+    //     return `这个操作返回#${id}cat`;
+    // }
+
+    // Async / await
+    // @Get()
+    // async findAll(): Promise<any[]> {
+    //     return [];
+    // }
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        return '这个操作添加新的cat';
+    }
+}
